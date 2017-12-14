814539a2a74aca2d8b4f339f1785a353db1ee4b5
Tyler R Thompson <thotyl88@gmail.com>
2017-12-14 18:46:26 -0500

    Added basic items
    Created basic pothole spawner that makes pothols at random positions within it's width. Then basic car object, still working on mouse movement.

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+	public float maxDistanceDelta = 1f;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		MoveCar();
+		Debug.Log("Car Position: " + transform.position);
+
+	}
+
+	private void MoveCar()
+	{
+
+		transform.position = Vector3.MoveTowards(transform.position, Input.mousePosition, maxDistanceDelta * Time.deltaTime);
+	}
+}
---
+++ Assets/PotholeSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotholeSpawner : MonoBehaviour
+{
+
+	public float width = 5f;
+	public float height = 5f;
+
+	public GameObject potholePrefab;
+	public float potholeSpeed = 5f;
+	public float potsPerSecond = 1f;
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			print("Pothole Spawn button");
+			CreatePothole();
+		}
+		float probability = Time.deltaTime * potsPerSecond;
+		if (UnityEngine.Random.value < probability)
+		{
+			CreatePothole();
+		}
+	}
+
+	private void CreatePothole()
+	{
+		//gets a random value between either end of the width property
+		float potholeRandomPos = UnityEngine.Random.Range(-width / 2, width / 2);
+		//creates spawn postion based on random value
+		Vector3 potholeSpawnPosition = new Vector3(potholeRandomPos, transform.position.y, transform.position.z);
+		//creates the pothole instance and gives it a velocity downwards
+		GameObject potholeInstance = Instantiate(potholePrefab, potholeSpawnPosition, Quaternion.identity);
+		potholeInstance.GetComponent<Rigidbody2D>().velocity = Vector3.down * potholeSpeed;
+
+	}
+
+	//Puts wire cube on spawner
+	public void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+	}
+}
